ab45aec3291bda01b80f9161297aa585cd2cef5b
Alice Marique <mariquealice83@gmail.com>
2024-04-04 15:40:45 +0200

    test abstractcontroller

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        // return $this->json([
+        //     'controller_name' => 'BlogController',
+        // ]);
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,56 +1,63 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/blog', name: 'blog_')]
 
-class DefaultController {
-    public function index(Request $request){
-        // dd($request);
-        $response = new Response('<h1>Hello World</h1>');
-        return $response;
+
+class DefaultController extends AbstractController {
+    #[Route('/')]
+    public function index(){
+        // retourne un fichier
+        // return $this->file('test.txt'); 
+
+        // return un fichier twig
+        return $this->render("base.html.twig");
+        
+
     }
 
 
-    #[
-        Route(
-            path: '/{name}',
-            name: 'blog',
-            methods: ["GET"],
-            schemes:["HTTPS"],
-            defaults: [
-                "name" => "",
-                "foo" => "bar"
-            ],
-            // requirements: [
-            //     'name' => '[a-zA-Z]'
-            // ]
+    // #[
+    //     Route(
+    //         path: '/{name}',
+    //         name: 'blog',
+    //         methods: ["GET"],
+    //         schemes:["HTTPS"],
+    //         defaults: [
+    //             "name" => "",
+    //             "foo" => "bar"
+    //         ],
+    //         // requirements: [
+    //         //     'name' => '[a-zA-Z]'
+    //         // ]
 
-            )
-    ]
-    public function blog(Request $request){
-        $title = $request->attributes->get('id');
-        // $allRouteparams = $request->attributes->all();
-        // $allparam = $request->attributes->get('_route_params');
-        dd($request);
-        return new Response('Blog');
-    }
+    //         )
+    // ]
+    // public function blog(Request $request){
+    //     $title = $request->attributes->get('id');
+    //     // $allRouteparams = $request->attributes->all();
+    //     // $allparam = $request->attributes->get('_route_params');
+    //     dd($request);
+    //     return new Response('Blog');
+    // }
 
     
-    #[
-        Route(
-            path: '/homepage',
-            name: 'homepage',
-            methods: ["GET"],
-            priority: 1
-            )
-    ]
+    // #[
+    //     Route(
+    //         path: '/homepage',
+    //         name: 'homepage',
+    //         methods: ["GET"],
+    //         priority: 1
+    //         )
+    // ]
 
-    public function blogHomePage(){
-        return new Response('blog homepage');
-    }
+    // public function blogHomePage(){
+    //     return new Response('blog homepage');
+    // }
 
 }
