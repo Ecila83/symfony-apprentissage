86ef98cea0626438b0b2c5c87bb3e9c46814be52
Alice Marique <mariquealice83@gmail.com>
2024-04-23 17:15:57 +0200

    cookies

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\TodoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +18,8 @@ class DefaultController extends AbstractController
   #[Route('/', name: 'index')]
   public function index(Request $request, RequestStack $requestStack)
   {
-
+    $cookie= Cookie::create('Cook',123);
+    $cookie->withHttpOnly(true);
     $todo = new Todo();
     $form = $this->createForm(TodoType::class, $todo);
 
@@ -29,9 +31,12 @@ class DefaultController extends AbstractController
       $this->addFlash('error', 'La todo n\'est pas valide');
     }
 
-    return $this->render('page1.html.twig', [
+    $reponse = $this->render('page1.html.twig', [
       'myform' => $form->createView()
     ]);
+    $reponse->headers->setCookie($cookie);
+
+    return $reponse;
   }
 }
 
